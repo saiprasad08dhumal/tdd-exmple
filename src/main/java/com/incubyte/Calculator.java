0d6823f7ebb69dd14493babe496f43692a0bb7c1
saiprasad <saiprasad08dhumal@gmail.com>
2020-06-15 14:15:20 +0530

    all test cases added

--- src/main/java/com/incubyte/Calculator.java
+++ src/main/java/com/incubyte/Calculator.java
@@ -3,17 +3,56 @@
  */
 package com.incubyte;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+
 /**
  * @author saiprasad
  *
  */
 public class Calculator {
 
+
 	public Integer add(String numbers) {
-		if(numbers == null || numbers.isEmpty()) {
-			return 0;
+		try {
+			if(numbers == null || numbers.isEmpty()) {
+				return 0;
+			}
+			Integer result = 0;			
+			List<String> numberList = createList(numbers);
+
+			if(numberList != null) {
+				List<String> negatives = numberList.stream()
+						.filter(n-> Integer.parseInt(n)<0).collect(Collectors.toList());
+				if(negatives != null && negatives.size()>1) {
+					throw new RuntimeException("negatives not allowed: "+String.join(",",negatives));
+				}
+				result = numberList.stream().mapToInt(n-> Integer.parseInt(n) ).sum();
+			}
+			return result;
 		}
-		return null;
+		catch(NumberFormatException ex) {
+			throw new RuntimeException("invalid argument passed: "+numbers);
+		}
+	}
+
+	private List<String> createList(String text) {
+		if(text.startsWith("//")) {
+			//split using custom delimiter:
+			Matcher m = Pattern.compile("//(.*)\n(.*)").matcher(text);
+			m.matches();
+			String customDelimiter = m.group(1);
+			String numbers = m.group(2);
+			String []arr = numbers.split(Pattern.quote(customDelimiter));
+			return Arrays.asList(arr);		
+		} else {
+			String []arr = text.split(",|\n");
+			return Arrays.asList(arr);	
+		}
 	}
 
 }
--- src/test/java/com/incubyte/CalculatorTest.java
+++ src/test/java/com/incubyte/CalculatorTest.java
@@ -1,6 +1,8 @@
 package com.incubyte;
 
 import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 class CalculatorTest {
@@ -9,7 +11,33 @@ class CalculatorTest {
 
 	@Test
 	void testEmpty() {
-		assertEquals(0, calculator.add(""));
+		assertEquals(0, calculator.add(null));
+	}
+	
+	@Test
+	void testMoreArgs() {
+		assertEquals(6, calculator.add("1,2,3"));
+	}
+	
+	@Test
+	void testMoreDelimiters() {
+		assertEquals(6, calculator.add("1\n2,3"));
+	}
+	@Test
+	void testCustomDelimiter() {
+		assertEquals(3, calculator.add("//;\n1;2"));
+	}
+	@Test
+	void negativeNotAllowedtest() {
+		 RuntimeException thrown =	Assertions.assertThrows(
+		           RuntimeException.class,
+		           ()->calculator.add("1,-2,-3")
+		    );
+		    assertTrue(thrown.getMessage().contains("negatives not allowed: -2,-3"));		
+	}
+	@Test
+	void longerDelimiterTest() {		
+		assertEquals(6, calculator.add("//***\n1***2***3"));
 	}
 
 }
